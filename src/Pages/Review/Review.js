a953e2bc29478873407444d6a08285729ad79c8e
Sumankdatta <sumankdatta18@gmail.com>
2022-11-11 19:00:40 +0600

    view added

--- src/Pages/Review/Review.js
+++ src/Pages/Review/Review.js
@@ -2,46 +2,56 @@ import React, { useContext, useEffect, useState } from 'react';
 import { AuthContext } from '../../context/AuthProvider/AuthProvider';
 import ReviewRow from '../ReviewRow/ReviewRow';
 
-
 const Review = () => {
 
     const { user } = useContext(AuthContext);
-    const [reviews, setReviews] = useState({})
+    const [reviews, setReviews] = useState({});
 
-console.log(reviews)
     useEffect(() => {
         fetch(`http://localhost:5000/reviews?email=${user.email}`)
-            .then(res => res.json())
-            .then(data => setReviews(data))
-    }, [user?.email])
+        .then(res=>res.json())
+        .then(data=>setReviews(data))
+    }, [user.email])
 
     return (
         <div>
-            <h1>review {reviews.length}</h1>
+            <h1>your reviews{reviews.length}</h1>
             <div className="overflow-x-auto w-full">
-                <table className="table w-full">
+  <table className="table w-full">
+   
+    <thead>
+      <tr>
+        <th>
+          <label>
+            <input type="checkbox" className="checkbox" />
+          </label>
+        </th>
+        <th>Name</th>
+        <th>Job</th>
+        <th>Favorite Color</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+     
+ {
+    reviews.map(rew=><ReviewRow></ReviewRow>)
+ }
 
-                    <thead>
-                        <tr>
-                            <th>
-                                <label>
-                                    <input type="checkbox" className="checkbox" />
-                                </label>
-                            </th>
-                            <th>Location</th>
-                            <th>Name</th>
-                            <th>Email</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-
-                    
-
-                    </tbody>
-
-                </table>
-            </div>
+    </tbody>
+   
+    <tfoot>
+      <tr>
+        <th></th>
+        <th>Name</th>
+        <th>Job</th>
+        <th>Favorite Color</th>
+        <th></th>
+      </tr>
+    </tfoot>
+    
+  </table>
+</div>
         </div>
     );
 };
--- src/Pages/ReviewRow/ReviewRow.js
+++ src/Pages/ReviewRow/ReviewRow.js
@@ -1,37 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const ReviewRow = ({revie}) => {
-    const {serviceName,customerName,email}=revie;
+const ReviewRow = ({ rew }) => {
+    const { service, serviceName, customerName, email, phone } = rew;
+const [view,setView]=useState({})
+    useEffect(() => {
+        fetch(`http://localhost:5000/services/${service}`)
+        .then(res=>res.json())
+        .then(data=>setView(data))
+    }, [service])
     return (
-        <tr>
-        <th>
-          <label>
-            <input type="checkbox" className="checkbox" />
-          </label>
-        </th>
-        <td>
-          <div className="flex items-center space-x-3">
-            <div className="avatar">
-              <div className="mask mask-squircle w-12 h-12">
-                <img src="/tailwind-css-component-profile-2@56w.png" alt="Avatar Tailwind CSS Component" />
-              </div>
-            </div>
-            <div>
-              <div className="font-bold">{serviceName}</div>
-              <div className="text-sm opacity-50"></div>
-            </div>
-          </div>
-        </td>
-        <td>
-          {customerName}
-          <br/>
-          <span className="badge badge-ghost badge-sm"></span>
-        </td>
-        <td>{email}</td>
-        <th>
-          <button className="btn btn-ghost btn-xs">details</button>
-        </th>
-      </tr>
+        <div>
+            <tr>
+                <th>
+                    <label>
+                        <input type="checkbox" className="checkbox" />
+                    </label>
+                </th>
+                <td>
+                    <div className="flex items-center space-x-3">
+                        <div className="avatar">
+                            <div className="mask mask-squircle w-12 h-12">
+                                <img src="" alt="Avatar Tailwind CSS Component" />
+                                {view.length}
+                            </div>
+                        </div>
+                        <div>
+                            <div className="font-bold">Hart Hagerty</div>
+                            <div className="text-sm opacity-50">United States</div>
+                        </div>
+                    </div>
+                </td>
+                <td>
+                    Zemlak, Daniel and Leannon
+                    <br />
+                    <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
+                </td>
+                <td>Purple</td>
+                <th>
+                    <button className="btn btn-ghost btn-xs">details</button>
+                </th>
+            </tr>
+        </div>
     );
 };
 
